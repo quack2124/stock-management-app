84a9506926ab294564f7107ca30747842db77e05
Djordje Dimitrijev <dimitrijevdjordje1@gmail.com>
2026-01-29 12:15:33 +0100

    Setup ui tests env and add test for add supplier form validation

---
+++ app/src/androidTest/java/com/app/stockmanagement/presentation/supplier/AddSupplierFragmentTest.kt
@@ -0,0 +1,42 @@
+package com.app.stockmanagement.presentation.supplier
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.app.stockmanagement.R
+import com.app.stockmanagement.presentation.supplier.add_supplier.AddSupplierFragment
+import launchFragmentInHiltContainer
+import org.hamcrest.core.IsNot.not
+import org.junit.Test
+
+class AddSupplierFragmentTest {
+
+    @Test
+    fun addSupplierFormShouldValidateInput() {
+        launchFragmentInHiltContainer<AddSupplierFragment>()
+
+        onView(withId(R.id.name)).perform(
+            typeText("Global supplier"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.contactPerson)).perform(
+            typeText("Jane doe"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.phone)).perform(typeText("123123"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.email)).perform(
+            typeText("jane@doe.com"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.action_save)).check(matches(not(isEnabled())))
+
+        onView(withId(R.id.address)).perform(
+            typeText("Example blvd 1"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.action_save)).check(matches(isEnabled()))
+    }
+}
---
+++ app/src/debug/java/com/app/stockmanagement/presentation/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.app.stockmanagement.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
